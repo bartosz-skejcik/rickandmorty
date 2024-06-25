b07c0689e65dbe55f8ac091d0a4e9b64e14301aa
Bartłomiej Paczesny <bartek@paczesny.pl>
2024-06-25 23:29:50 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full flex sm:flex-row flex-col justify-between items-center p-2 bg-black">
+      <p className="text-lg font-medium text-white">
+        Made by Jan Sosnowski
+      </p>
+      <p className="text-base font-medium text-neutral-700">
+        jan13sosnowski@gmail.com
+      </p>
+    </footer>
+  )
+};

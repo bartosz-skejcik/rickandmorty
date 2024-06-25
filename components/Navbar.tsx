1f45523d3cc459af215a7f660bc9c67645d910e9
Bartłomiej Paczesny <bartek@paczesny.pl>
2024-06-25 23:24:19 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -58,7 +58,7 @@ export default function NavBar() {
                         as={Link}
                         size="sm"
                         color="warning"
-                        href="https://dev.paczesny.pl"
+                        href="#"
                         variant="flat"
                     >
                         Contact
